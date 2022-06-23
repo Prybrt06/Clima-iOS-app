29af0fe9f14da1fa93e1438ec3186f72faf254a5
Prybrt06 <prybrt06@gmail.com>
2022-06-23 18:11:27 +0530

    A new file named WeatherModel is uploaded

---
+++ WeatherModel.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+struct WeatherModel
+{
+    let conditionID: Int
+    let city: String
+    let temperature: Double
+    
+    var conditionName: String
+    {
+        switch conditionID
+        {
+        case 200...232:
+            return "cloud.bolt"
+            
+        case 300...321:
+            return "cloud.drizzle"
+            
+        case 500...531:
+            return "cloud.rain"
+            
+        case 600...622:
+            return "cloud.snow"
+            
+        case 701...781:
+            return "cloud.fog"
+            
+        case 800:
+            return "sun.max"
+            
+        case 801...804:
+            return "cloud.bolt"
+            
+        default:
+            return "cloud"
+        }
+    }
+}
